d39ea35a5a0d451e5b855dcb33e51dadade9ac57
João Filho <117484929+joaofilhox@users.noreply.github.com>
2023-10-31 14:58:34 -0300

    Update agenciaDeViagens.sql
     Corrigi o nome do banco de dados.

--- modelagemDeDados/agenciaDeViagens.sql
+++ modelagemDeDados/agenciaDeViagens.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE agenciaDeViagem;
+CREATE DATABASE agenciaDeViagens;
 
 CREATE TABLE Cliente (
     idCliente INT AUTO_INCREMENT PRIMARY KEY,
@@ -12,16 +12,15 @@ CREATE TABLE Cliente (
 CREATE TABLE Pacotes_de_Interesse (
     idPacoteDeViagens INT AUTO_INCREMENT PRIMARY KEY,
     destinos VARCHAR(255),
-    precos DECIMAL(10, 2),
+    precos VARCHAR(255),  
     idCliente INT,
     FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente)
 );
 
 CREATE TABLE Pagamentos (
     idPagamento INT AUTO_INCREMENT PRIMARY KEY,
-    valorPago DECIMAL(10, 2),
+    valorPago VARCHAR(255),  
     destinoPago VARCHAR(255),
     idCliente INT,
     FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente)
 );
-
